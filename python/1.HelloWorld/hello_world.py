60a29a734d8e906abbc492c240b537eedf36882a
davidkan <davidkan717@gmail.com>
2025-04-18 03:31:08 -0400

    python 1.HelloWorld

--- python/1.HelloWorld/hello_world.py
+++ python/1.HelloWorld/hello_world.py
@@ -9,11 +9,11 @@ sfda
 sdf
 a
 """
-
-print("HelloWorld")
-integer: int = 2
-print("This is an integer: ", integer)
-string: str = "Hello World"
-print("This is an string: ", string)
-isTrue: bool = True
-print("This is a boolean: ", isTrue)
+def main():
+    print("HelloWorld")
+    integer: int = 2
+    print("This is an integer: ", integer)
+    string: str = "Hello World"
+    print("This is an string: ", string)
+    isTrue: bool = True
+    print("This is a boolean: ", isTrue)
