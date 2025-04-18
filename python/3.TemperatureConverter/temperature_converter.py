73b0d844bc7429f4d0e80eabf87882a50bd284c3
davidkan <davidkan717@gmail.com>
2025-04-18 03:21:14 -0400

    python 3.TemperatureConverter

--- python/3.TemperatureConverter/temperature_converter.py
+++ python/3.TemperatureConverter/temperature_converter.py
@@ -4,16 +4,22 @@ Take user input via input()
 Convert between Celsius and Fahrenheit
 Use functions to organize logic
 """
+
+def f_to_c(f : float):
+    print((f - 32) * (5 / 9))
+def c_to_f(c : float):
+
+    print(c * (9 / 5) + 32)
+
 run = True
 while (run):
     conversion = input("F->C or C->F? (Type 1 or 2) type q to quit:")
     if conversion == "1": # F->C
-        f: float = float(input("Input your temperature in Fahrenheit: "))
-        print((f - 32) * (5/9))
+        f_to_c(float(input("Input your temperature in Fahrenheit: ")))
     elif conversion == "2":
-        c : float = float(input("Input your temperature in Celsius: "))
-        print(c * (9/5) + 32)
+        c_to_f(float(input("Input your temperature in Celsius: ")))
     elif conversion == "q":
         run = False
     else:
         print("Invalid input")
+
